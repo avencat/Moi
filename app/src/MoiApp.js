a062d6395cad6322d97eafeee7f5453aa43071d9
Arnaud <arnaud.thomazo@epitech.eu>
2018-11-03 15:28:54 +0100

    Init react-native-language

--- app/src/MoiApp.js
+++ app/src/MoiApp.js
@@ -4,7 +4,9 @@
  * The root element
  */
 import * as React from 'react';
+import RNLanguages from 'react-native-languages';
 import { Platform, Text, View } from 'react-native';
+import i18n from '@resources/translations';
 import styles from './MoiAppStyles';
 
 const instructions = Platform.select({
@@ -12,10 +14,26 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
 });
 
-export default () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-    <Text style={styles.instructions}>To get started, edit App.js</Text>
-    <Text style={styles.instructions}>{instructions}</Text>
-  </View>
-);
+export default class MoiApp extends React.PureComponent {
+  componentDidMount() {
+    RNLanguages.addEventListener('change', this.onLanguagesChange);
+  }
+
+  componentWillUnmount() {
+    RNLanguages.removeEventListener('change', this.onLanguagesChange);
+  }
+
+  onLanguagesChange = ({ language }) => {
+    i18n.locale = language;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
